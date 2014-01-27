bbcd28b328aac4c658723b83d4003977b97962bd
Ed Langley <ed@edlangley.co.uk>
2014-01-27 20:37:48 +0000

    Add skeleton Qt C++ project
    With the default qmlapplicationviewer files to load the QtQuick 1.1 GUI.

---
+++ backupdoit/main.cpp
@@ -0,0 +1,14 @@
+#include <QApplication>
+#include "qmlapplicationviewer.h"
+
+Q_DECL_EXPORT int main(int argc, char *argv[])
+{
+    QScopedPointer<QApplication> app(createApplication(argc, argv));
+
+    QmlApplicationViewer viewer;
+    viewer.setOrientation(QmlApplicationViewer::ScreenOrientationAuto);
+    viewer.setMainQmlFile(QLatin1String("qml/backupdoit_qml/backupdoit.qml"));
+    viewer.showExpanded();
+
+    return app->exec();
+}
